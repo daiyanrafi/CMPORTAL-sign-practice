54dee92df9f3e647103522728d01a19c9ac0201e
raf <daiyanrafi1@gmail.com>
2024-02-08 11:56:55 +0600

    pu

--- CMPORTAL/modules/Booking-table/dataService.ts
+++ CMPORTAL/modules/Booking-table/dataService.ts
@@ -1,3 +1,33 @@
+// // dataService.ts
+
+// interface BookingData {
+//     bookableresourcebookingid: string;
+//     resource: string;
+//     starttime: string;
+//     endtime: string;
+//     duration: number;
+//     bookingtypetext: string;
+//     bookingstatus: string;
+//     createdon: string;
+//   }
+  
+//   const fetchData = async (): Promise<BookingData[]> => {
+//     try {
+//       const response = await fetch('/path/to/your/livedata.json');
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch data');
+//       }
+//       const data = await response.json();
+//       return data as BookingData[];
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//       return [];
+//     }
+//   };
+  
+//   export default fetchData;
+  
+
 // dataService.ts
 
 import livedata from './livedata.json';
